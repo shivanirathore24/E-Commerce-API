3970cc50c12c580719da76c3efcf63cfb33f2d61
shivanirathore24 <shivani.rathore2409@gmail.com>
2025-03-15 21:51:56 +0530

    Added: Winston Middleware

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,20 +1,28 @@
 import fs from "fs";
 const fsPromise = fs.promises;
+import winston from "winston";
 
-async function log(logData) {
-  try {
-    logData = `${new Date().toString()} +  ${logData}\n`;
-    await fsPromise.appendFile("log.txt", logData);
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function log(logData) {
+//   try {
+//     logData = `${new Date().toString()} +  ${logData}\n`;
+//     await fsPromise.appendFile("log.txt", logData);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "request-logging" },
+  transports: [new winston.transports.File({ filename: "log.txt" })],
+});
 
 const loggerMiddleware = async (req, res, next) => {
   // Exclude logging for /signin and /signup routes
   if (!req.url.includes("/signin") && !req.url.includes("/signup")) {
     const logData = `${req.url} - ${JSON.stringify(req.body)}`;
-    await log(logData);
+    logger.info(logData);
   }
   next();
 };
